57307fce7ea1f0cabff2bcb1e2a67a7861747a13
Omar Juma <jumao2669@gmail.com>
2023-06-20 22:39:50 +0300

    connected to db

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 // const { createHandler } = require("graphql-http/lib/use/express");
 const { graphqlHTTP } = require("express-graphql");
+const mongoose = require("mongoose");
 const schema = require("./schema/schema");
 const app = express();
 
+mongoose.connect("mongodb+srv://devjuma03:omarionJumah@cluster0.wh5jcsc.mongodb.net/")
+mongoose.connection.once("open", () => {
+  try{
+    console.log("Connected to database");
+  }
+catch(err){
+  console.log(err);
+}
+});
 app.use("/graphql", graphqlHTTP({
      schema,
      graphiql : true,
