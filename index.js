e8d36a32737658d6e8f8f53f163c5f8c1aedd6e2
Omar Juma <jumao2669@gmail.com>
2023-06-20 12:27:23 +0300

    I learnt to set up the schema and root query

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+// const { createHandler } = require("graphql-http/lib/use/express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const app = express();
+
+app.use("/graphql", graphqlHTTP({
+     schema,
+     graphiql : true,
+    }));
+
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
